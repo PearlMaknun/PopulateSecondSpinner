0d23d3245ee433414513a0f14d2d14d289d4ede5
PearlMaknun <almaknunlulu6@gmail.com>
2018-11-21 14:56:55 +0700

    Initial commit

---
+++ app/src/main/java/io/github/pearlmaknun/populatesecondspinner/MainActivity.java
@@ -0,0 +1,62 @@
+package io.github.pearlmaknun.populatesecondspinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
+
+    Spinner s1,s2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        s1 = findViewById(R.id.spinner1);
+        s2 = findViewById(R.id.spinner2);
+        s1.setOnItemSelectedListener(this);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                               long arg3) {
+        // TODO Auto-generated method stub
+        String sp1= String.valueOf(s1.getSelectedItem());
+        Toast.makeText(this, sp1, Toast.LENGTH_SHORT).show();
+        if(sp1.contentEquals("A Kategori")) {
+            List<String> list = new ArrayList<String>();
+            list.add("A Item 1");
+            list.add("A Item 2");
+            list.add("A Item 3");
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, list);
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter.notifyDataSetChanged();
+            s2.setAdapter(dataAdapter);
+        }
+        if(sp1.contentEquals("B Kategori")) {
+            List<String> list = new ArrayList<String>();
+            list.add("B Item 1");
+            list.add("B Item 2");
+            list.add("B Item 3");
+            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, list);
+            dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter2.notifyDataSetChanged();
+            s2.setAdapter(dataAdapter2);
+        }
+
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
+    }
+}
